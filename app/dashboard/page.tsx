9f6cfb71b52a0a03b1e4f64f622151c22b779c4d
pawansuthar <pawansuthar15372097023627153@gmail.com>
2024-05-01 19:54:47 +0530

    initial commit

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+const dashboard = () => {
+  return (
+    <Button>Button</Button>
+    )
+}
+
+export default dashboard
